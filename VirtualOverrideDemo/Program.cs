0c6d998a2f219886c80eca652ee69a81967fb79c
kacperkwa <kacperkwartaxz@gmail.com>
2023-06-30 13:41:38 +0200

    Add project files.

---
+++ VirtualOverrideDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace VirtualOverrideDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("Mella",10);
+            Console.WriteLine($"{dog.Name} is {dog.Age} years old");
+            dog.Play();
+            dog.Eat();
+            dog.MakeSound();
+
+            Console.ReadKey();
+        }
+    }
+}
